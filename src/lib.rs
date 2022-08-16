aaf34d4b3db7deabad94e22ab9a20c0ed35349a5
Andy Levenson <andrew.levenson@gmail.com>
2022-08-15 20:52:19 -0400

    More tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@ pub mod util;
 #[cfg(test)]
 mod tests {
     use crate::quicksearch::QuickSearch;
-    use crate::string_sim::jaro_winkler;
-    use crate::util::lines_from_file;
+    use crate::string_sim::{jaro_winkler, ngram_jaccard};
+    use crate::util::{jaccard_similarity, lines_from_file};
+    use std::collections::HashSet;
     #[test]
     fn lines_from_file_works() {
         let names = lines_from_file(r"fake_names.txt");
@@ -26,6 +27,28 @@ mod tests {
     }
     #[test]
     fn jaro_winkler_works() {
-        assert!((jaro_winkler("Trapeze", "Trace") * 1000.0).round() == 853.0)
+        assert!((jaro_winkler("Trapeze", "Trace") * 100.0).round() == 85.0)
+    }
+    #[test]
+    fn jaccard_works() {
+        let a = HashSet::from([1, 2]);
+        let b = HashSet::from([1, 2, 3]);
+
+        assert!((jaccard_similarity(&a, &b) * 100.0).round() == 67.0);
+    }
+    #[test]
+    fn ngram_jaccard_works() {
+        let a = "abc";
+        let b = "abc";
+
+        assert!(ngram_jaccard(a, b, 1) == 1.0);
+        assert!(ngram_jaccard(a, b, 2) == 1.0);
+        assert!(ngram_jaccard(a, b, 3) == 1.0);
+
+        let a = "abc";
+        let b = "abcabc";
+        assert!(ngram_jaccard(a, b, 1) == 1.0);
+        assert!((ngram_jaccard(a, b, 2) * 100.0).round() == 67.0);
+        assert!((ngram_jaccard(a, b, 3) * 100.0).round() == 33.0);
     }
 }
